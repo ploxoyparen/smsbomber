5352619784a03eec2f7d0af8912441d680e04018
ploxoyparen <104434202+ploxoyparen@users.noreply.github.com>
2022-04-27 01:27:19 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+token = '5366986895:AAEsIEdbCxiOT5L2EOcd3JI4x9-u2W_ZOhA' # Токен бота с @BotFather
+
+QIWI_PUBLICKEY = '48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPvYU2XdcwzvCq1gsYtYJT4VMstbsa7D6kUvQdyAbwBDMXfJYHBUn47c7f5754AtfFFFuaLUKZhyJSdvdDzqNoZKDnKvRhWHBkK2xGQsYCD' # Сюда P2P Public Key, брать тут: qiwi.com/p2p-admin/transfers/api
+QIWI_TOKEN = 'b0990b37fbd27b841fea685dcbcbb61f' # Токен киви, брать тут: qiwi.com/api
+QIWI_NUMBER = '79213608035' # Номер киви, без +
+
+admin_id = 1681518154 # Ваш ID, брать тут: @getmyid_bot
+
+price = 99 # Цена "подписки"
+
